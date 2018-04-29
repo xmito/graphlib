7bdacdc1e7da1e85de256ad7aade6f48947751bd
Matej Hulín <mito.hulin@gmail.com>
2018-04-30 01:29:49 +0200

    BellmanFord can now work on undirected graphs with no negative edges

--- BellmanFord.h
+++ BellmanFord.h
@@ -16,22 +16,42 @@ std::enable_if_t<Graph::directedTag &&
 bellmanFord(Graph &graph,
             const typename graph_traits<Graph>::node_handle &source) {
 	using weight_type = typename graph_traits<Graph>::weight_type;
+	using distance_type = typename graph_traits<Graph>::distance_type;
 	initializeSingleSource(graph, source);
 	size_t nonodes = graph.nodeCount();
 	while(--nonodes) {
-		for (auto eit = graph.beginEdge(); eit != graph.endEdge(); ++eit) {
+		for (auto eit = graph.beginEdge(); eit != graph.endEdge(); ++eit)
 			relax(graph, *eit);
-		}
 	}
 	for (auto eit = graph.beginEdge(); eit != graph.endEdge(); ++eit) {
 		weight_type weight = graph.getWeight(*eit);
 		auto &src_data = graph.getSourceNode(*eit);
 		auto &tg_data = graph.getTargetNode(*eit);
-		if (src_data.dist_ != LLONG_MAX && tg_data.dist_ > src_data.dist_ + weight)
+		if (src_data.dist_ != std::numeric_limits<distance_type>::max() &&
+		        tg_data.dist_ > src_data.dist_ + weight)
 			return false;
 	}
 	return true;
 }
+
+template<typename Graph>
+std::enable_if_t<!Graph::directedTag &&
+                 Graph::weightedTag &&
+                 Graph::pathTag, bool>
+bellmanFord(Graph &graph,
+            const typename graph_traits<Graph>::node_handle &source) {
+	initializeSingleSource(graph, source);
+	size_t nonodes = graph.nodeCount();
+	while(--nonodes) {
+		for (auto eit = graph.beginEdge(); eit != graph.endEdge(); ++eit) {
+			    if (graph.getWeight(*eit) < 0)
+					return false;
+			relax(graph, *eit);
+		}
+	}
+	return true;
+}
+
 
 }
 #endif
--- Relax.h
+++ Relax.h
@@ -2,6 +2,7 @@
 #define RELAX_H
 #include <type_traits>
 #include <climits>
+#include <limits>
 #include "GraphTraits.h"
 
 namespace graphlib {
@@ -11,13 +12,38 @@ std::enable_if_t<Graph::pathTag && Graph::directedTag>
 relax(Graph &graph,
       const typename graph_traits<Graph>::edge_handle &eh) {
 	using weight_type = typename graph_traits<Graph>::weight_type;
+	using distance_type = typename graph_traits<Graph>::distance_type;
 	auto src_nh = graph.getSource(eh);
 	auto &src_data = graph.getSourceNode(eh);
 	auto &tg_data = graph.getTargetNode(eh);
 	weight_type weight = graph.getWeight(eh);
-	if (src_data.dist_ != LLONG_MAX && tg_data.dist_ > src_data.dist_ + weight) {
+	if (src_data.dist_ != std::numeric_limits<distance_type>::max() &&
+	        tg_data.dist_ > src_data.dist_ + weight) {
 		tg_data.dist_ = src_data.dist_ + weight;
 		tg_data.pred_ = src_nh;
+	}
+}
+
+template<typename Graph>
+std::enable_if_t<Graph::pathTag && !Graph::directedTag>
+relax(Graph &graph,
+      const typename graph_traits<Graph>::edge_handle &eh) {
+	using weight_type = typename graph_traits<Graph>::weight_type;
+	using distance_type = typename graph_traits<Graph>::distance_type;
+	auto [fst, snd] = graph.getBoth(eh);
+	auto &fst_data = graph.getNode(fst);
+	auto &snd_data = graph.getNode(snd);
+	weight_type weight = graph.getWeight(eh);
+	/* Undirected edges are basically two directed edges */
+	if (fst_data.dist_ != std::numeric_limits<distance_type>::max() &&
+	        snd_data.dist_ > fst_data.dist_ + weight) {
+		snd_data.dist_ = fst_data.dist_ + weight;
+		snd_data.pred_ = fst;
+	}
+	if (snd_data.dist_ != std::numeric_limits<distance_type>::max() &&
+	        fst_data.dist_ > snd_data.dist_ + weight) {
+		fst_data.dist_ = snd_data.dist_ + weight;
+		fst_data.pred_ = snd;
 	}
 }
 
