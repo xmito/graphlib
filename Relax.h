965ada8fa3b843f41522b29901e890606fa37254
Matej Hulín <mito.hulin@gmail.com>
2018-05-04 22:59:39 +0200

    Relax function for dijkstra

--- Relax.h
+++ Relax.h
@@ -8,7 +8,7 @@
 namespace graphlib {
 
 template<typename Graph>
-std::enable_if_t<Graph::pathTag && Graph::directedTag>
+std::enable_if_t<Graph::pathTag && Graph::directedTag, bool>
 relax(Graph &graph,
       const typename graph_traits<Graph>::edge_handle &eh) {
 	using weight_type = typename graph_traits<Graph>::weight_type;
@@ -21,9 +21,13 @@ relax(Graph &graph,
 	        tg_data.dist_ > src_data.dist_ + weight) {
 		tg_data.dist_ = src_data.dist_ + weight;
 		tg_data.pred_ = src_nh;
+		return true;
 	}
+	return false;
 }
 
+/* relax for undirected graphs, which relaxes undirected edge
+ * in both directions. This is useful for BellmanFord implementation */
 template<typename Graph>
 std::enable_if_t<Graph::pathTag && !Graph::directedTag>
 relax(Graph &graph,
@@ -45,6 +49,29 @@ relax(Graph &graph,
 		fst_data.dist_ = snd_data.dist_ + weight;
 		fst_data.pred_ = snd;
 	}
+}
+
+/* relax for undirected graphs, predominantly used by dijkstra shortest
+ * path algorithm, which requires relaxation of undirected edge only in
+ * one direction */
+template<typename Graph>
+std::enable_if_t<Graph::pathTag &&
+                 !Graph::directedTag, bool>
+relax(Graph &graph,
+      const typename graph_traits<Graph>::edge_handle &eh,
+      const typename graph_traits<Graph>::node_handle &src) {
+	using weight_type = typename graph_traits<Graph>::weight_type;
+	using distance_type = typename graph_traits<Graph>::distance_type;
+	auto &src_data = graph.getNode(src);
+	auto &tg_data = graph.getOtherNode(eh, src);
+	weight_type weight = graph.getWeight(eh);
+	if (src_data.dist_ != std::numeric_limits<distance_type>::max() &&
+	        tg_data.dist_ > src_data.dist_ + weight) {
+		tg_data.dist_ = src_data.dist_ + weight;
+		tg_data.pred_ = src;
+		return true;
+	}
+	return false;
 }
 
 }
