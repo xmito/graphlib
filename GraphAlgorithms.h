0f0e83e1c0e1120b83e031603c735b0f9aeaac37
Matej Hulín <mito.hulin@gmail.com>
2018-05-05 13:47:36 +0200

    Update for GraphAlgorithms and CMakeLists

--- GraphAlgorithms.h
+++ GraphAlgorithms.h
@@ -16,10 +16,12 @@ std::enable_if_t<Graph::traversableTag>
 dfs(Graph& graph);
 
 template<typename Graph>
-std::enable_if_t<Graph::traversableTag || Graph::pathTag>
+std::enable_if_t<Graph::traversableTag>
 bfs(Graph& graph,
     const typename graph_traits<Graph>::node_handle &source);
 
+/* Iterative deepening DFS - function traverses graph to depth
+ * max_depth and returns true, if it finds goal_nh */
 template<typename Graph>
 std::enable_if_t<Graph::traversableTag, bool>
 iddfs(Graph& graph,
@@ -29,8 +31,7 @@ iddfs(Graph& graph,
 
 template<typename Graph,
          typename Heuristic,
-         typename PriorityQueue = BinHeap<typename graph_traits<Graph>::node_handle,
-                                          CustomComparator<Graph>>>
+         typename PriorityQueue = BinHeap<Graph>>
 void aStar(Graph& graph,
            const typename graph_traits<Graph>::node_handle &source,
            const typename graph_traits<Graph>::node_handle &target,
@@ -41,17 +42,21 @@ void aStar(Graph& graph,
  *** SHORTEST-PATH ALGORITHMS ***
  *******************************/
 
-/* Dijkstra for directed weighted graphs with no negative edges */
+/* Dijkstra implementation for both directed and undirected
+ * graphs. When Graph is undirected, its edges are treated
+ * as two directed and called relax function reflects this.
+ * Returned bool value signifies, whether graph contained
+ * negative edge, iow. whether dijkstra call was successful */
 template<typename Graph,
-         typename PriorityQueue = BinHeap<typename graph_traits<Graph>::node_handle,
-                                          CustomComparator<Graph>>>
-std::enable_if_t<Graph::directedTag &&
-                 Graph::weightedTag &&
-                 Graph::pathTag>
+         typename PriorityQueue = BinHeap<Graph>>
+std::enable_if_t<Graph::weightedTag &&
+                 Graph::pathTag, bool>
 dijkstra(Graph& graph,
          const typename graph_traits<Graph>::node_handle &source);
 
-// Bellman-Ford algorithm for directed weighted graphs
+/* Bellman-Ford algorithm for directed weighted graphs. Returns
+ * true if provided Graph argument didn't contain any negative
+ * weight cycle, iow. whether it successfully finished */
 template<typename Graph>
 std::enable_if_t<Graph::directedTag &&
                  Graph::weightedTag &&
@@ -59,9 +64,10 @@ std::enable_if_t<Graph::directedTag &&
 bellmanFord(Graph& graph,
             const typename graph_traits<Graph>::node_handle &source);
 
-/* Bellman-Ford algorithm that works on undirected
- * graphs by detecting negative edges, that trivially
- * comprise negative loops*/
+/* Bellman-Ford algorithm that works on undirected graphs
+ * by detecting negative edges, that trivially comprise
+ * negative loops. It returns true if provided Graph instance
+ * didn't contain negative edge and finished successfully */
 template<typename Graph>
 std::enable_if_t<!Graph::directedTag &&
                   Graph::weightedTag &&
@@ -69,24 +75,30 @@ std::enable_if_t<!Graph::directedTag &&
 bellmanFord(Graph& graph,
             const typename graph_traits<Graph>::node_handle &source);
 
-// DAG - finds shortest path in directed weighted acyclic graph
+/* DAG - find shortest paths from source node in directed
+ * acyclic graph. The return value of type bool signifies
+ * whether it finished successfully */
 template<typename Graph>
 std::enable_if_t<Graph::directedTag &&
                  Graph::weightedTag &&
-                 Graph::pathTag>
-dag(Graph& graph, typename graph_traits<Graph>::node_handle &source);
+                 Graph::pathTag, bool>
+dag(Graph& graph,
+    const typename graph_traits<Graph>::node_handle &source);
 
-/* Assumes directed weighted graphs with no negative weight cycles*/
+/* Floyd-Warshall - computes all-pairs shortest-paths matrix
+ * for directed weighted graphs */
 template<typename Graph>
 std::enable_if_t<Graph::directedTag &&
                  Graph::weightedTag &&
-                 Graph::pathTag, Matrix>
+                 Graph::pathTag, Matrix<typename graph_traits<Graph>::distance_type>>
 floydWarshall(const Graph& graph);
 
+/* Johnson - computes all-pairs shortest-paths matrix for
+ * directed weighted graphs */
 template<typename Graph>
 std::enable_if_t<Graph::directedTag &&
                  Graph::weightedTag &&
-                 Graph::pathTag, Matrix>
+                 Graph::pathTag, Matrix<typename graph_traits<Graph>::distance_type>>
 johnson(Graph& graph);
 
 }
