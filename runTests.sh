ce3bcd0db80221dbb11c5ee94083efdeb7732a13
Vojtěch Kaňa <xkana1@fi.muni.cz>
2018-05-06 02:06:57 +0200

    script for running all tests at once

---
+++ runTests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo $0
+scriptDir="${0%/*}"
+#run-parts "$scriptDir"/build/ - this works too, but it does not show proper colors
+list="$(find "$scriptDir"/build/ -maxdepth 1 -executable -type f)"
+for file in $list; do
+	$file
+	#eval $file > /dev/null
+	#if [ $? != 0  ]; then
+		#$file
+	#fi
+done
