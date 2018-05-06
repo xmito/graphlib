c4aefa2f643f655a5b34ad70d2865245e36d74af
Vojtěch Kaňa <xkana1@fi.muni.cz>
2018-05-06 02:23:20 +0200

    echo filename

--- runTests.sh
+++ runTests.sh
@@ -5,6 +5,7 @@ scriptDir="${0%/*}"
 #run-parts "$scriptDir"/build/ - this works too, but it does not show proper colors
 list="$(find "$scriptDir"/build/ -maxdepth 1 -executable -type f)"
 for file in $list; do
+	echo $file
 	$file
 	#eval $file > /dev/null
 	#if [ $? != 0  ]; then
