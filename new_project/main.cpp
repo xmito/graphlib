7d7aad425a56939697e431d6a6b8b0d101878f9b
Vojtech Kana <kana.vojtech@gmail.com>
2018-05-20 16:00:44 +0200

    tried linking

---
+++ new_project/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <graphlib.h>
+
+int main(int argc, char *argv[])
+{
+	graphlib::Matrix<int> mtx;
+	std::cout << mtx.empty() << std::endl;
+
+
+
+	std::cout << "do sth" << std::endl;
+	return 0;
+}
