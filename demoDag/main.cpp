ab82f6876d26099827ce4b4505e0d302590c81f5
Matej Hulín <mito.hulin@gmail.com>
2018-05-21 11:58:01 +0200

    Demo examples

---
+++ demoDag/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "graphlib.h"
+
+int main() {
+	using namespace graphlib;
+	using Graph = ListDiGraph<PathNodeData, WeightedEdgeData>;
+	using node_handle = typename graph_traits<Graph>::node_handle;
+	Graph graph;
+	node_handle handles[6];
+	for (int i = 0; i < 6; ++i)
+		handles[i] = graph.addNode();
+
+	graph.addEdge(handles[0], handles[1], 3);
+	graph.addEdge(handles[0], handles[2], 8);
+	graph.addEdge(handles[0], handles[4], 4);
+	graph.addEdge(handles[2], handles[1], 4);
+	graph.addEdge(handles[3], handles[2], 6);
+	graph.addEdge(handles[4], handles[3], 6);
+	graph.addEdge(handles[2], handles[5], 4);
+	graph.addEdge(handles[3], handles[5], 1);
+
+	graph.exportGraph("./graph.dot");
+	dag(graph, handles[0]);
+	graph.exportShortestPath("./graphsh.dot", handles[5]);
+	return 0;
+}
